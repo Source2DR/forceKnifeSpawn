0797b3f68b7cecd81dc433b0cb931edf2718a14b
Chase <itschase88@gmail.com>
2024-06-24 15:29:42 -0500

    1.0.2

--- forceKnifeSpawn.cs
+++ forceKnifeSpawn.cs
@@ -8,18 +8,20 @@ public class forceKnifeSpawn : BasePlugin {
     public override string ModuleName => "Force Knife Spawn";
     public override string ModuleAuthor => "Chase88";
     public override string ModuleDescription => "This is a simple plugin that will force all players to recieve a knife on spawn.";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.0.2";
 
     public override void Load(bool hotReload) {
         //Hook into the player spawn event
         RegisterEventHandler<EventPlayerSpawn>((@event, info) => {
             //Check to make sure our player is valid
-            if(@event == null || !@event.Userid.IsValid) {
+            if(@event == null || @event.Userid == null || @event.Userid.IsValid) {
                 return HookResult.Continue;
             }
 
-            //Give the player a knife
-            @event.Userid.GiveNamedItem("weapon_knife");
+            CCSPlayerController player = @event.Userid;
+
+            player.GiveNamedItem("weapon_knife");
+            player.GiveNamedItem("weapon_knife");
 
             return HookResult.Continue;
         });
